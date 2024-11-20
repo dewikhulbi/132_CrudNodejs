8b4de9152febe6e6dad4214921a68142923cde7e
dewikhulbi <dewikhlb20@gmail.com>
2024-11-20 10:44:27 +0700

    Middlewere

--- app.js
+++ app.js
@@ -1,25 +1,62 @@
 const express = require('express');
-const todoRoutes = require('./routes/tododb.js');
+const todoRoutes = require('./routes/todo.js');
+const expressLayout = require('express-ejs-layouts');
+const db = require('./database/db.js');
 const app = express();
+
+// Load environment variables from .env
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
+const session = require('express-session');
+const authRoutes = require('./routes/authRoutes');
+const { isAuthenticated } = require('./middlewares/middleware.js');
+const { title } = require('process');
+
+// Middleware and settings
+app.use(expressLayout);
 app.use(express.json());
-
-app.use('/todos', todoRoutes);
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('index');
+
+// Routes
+app.use('/todos', todoRoutes);
+
+app.use(express.urlencoded({ extended: true }));
+
+// Konfigurasi express-session
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false } // Set ke true jika menggunakan HTTPS
+}));
+
+app.use('/', authRoutes);
+
+app.get('/', isAuthenticated, (req, res) => {
+    res.render('index', {
+        layout: 'layouts/main-layout'
+    });
 });
 
-app.get('/contact', (req, res) => {
-    res.render('contact');
+app.get('/contact', isAuthenticated, (req, res) => {
+    res.render('contact', {
+        layout: 'layouts/main-layout',
+        title: 'Halaman Contact'
+    });
 });
 
-app.use((req, res) => {
-    res.status(404).send('404 - Page Not Found');
+app.get('/todo-view', isAuthenticated, (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
 });
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
